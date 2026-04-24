1358e2093fb430cf4a54f23c0d29538e20c09955
Rainer Staringer <rainer.staringer@ai.dlh.lu>
2026-04-24 06:48:26 +0000

    Made it work also when string is empty.

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in str[:-1]:
+    upper = ""
+    for c in str:
         if "a" <= c <= "z":
-            letter = ord("A") + ord(c) - ord("a")
+            upper += chr(ord("A") + ord(c) - ord("a"))
         else:
-            letter = ord(c)
-        print("{0:c}".format(letter), end="")
-    c = str[-1]
-    if "a" <= c <= "z":
-        letter = ord("A") + ord(c) - ord("a")
-    else:
-        letter = ord(c)
-    print("{0:c}".format(letter))
+            upper += c
+    print("{0}".format(upper))
